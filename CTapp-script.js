07572f17e64c978523f205ebf1ba79b94f4ee783
zion Niq <zion Niq>
2017-07-26 22:49:56 -0400

    <z-tabs> now communicates with <z-lists> via the ItemsManipulator factory to determine which list the user should be looking at

--- CTapp-script.js
+++ CTapp-script.js
@@ -5,117 +5,32 @@ CTapp.factory("ItemsManipulator",function(){                    //TODO TODO TODO
   var factory = {};
 
   factory.mode = "folders";
-  factory.folderC = 0;
   factory.folders = {};
-  factory.currFolderId ="";
-  factory.currList = factory.folders;
+  factory.currFolderId = "";
+  factory.currFolder = undefined;
+  factory.currRecordId = "";
+  factory.currRecord = undefined;
+  factory.validModes = ["folders","records","forms","void"];
 
-  factory.switchList = function(mode){
-    this.mode = mode;
-
-    if (this.mode == "folders"){
-      alert("in mode == folders");
-      this.currList = this.folders;
-
-    } else if (this.mode == "records"){
-      alert("in mode == records");
-      this.currList = this.folders[this.currFolderId].getRecords();
+  factory.setMode = function(mode){
+    if (this.validModes.contains(mode)){
+      //alert("mode is now == "+mode);
+      this.mode = mode;
 
     } else {
-      alert("invalid mode");
+      alert("invalid mode: "+mode);
     }
-    alert("done switching list");
+  }
+
+  factory.setCurrRecord = function(id,obj){
+
+  }
+
+  factory.setCurrFolder = function(id,obj){
+
   }
 
   return factory;
-})
-.directive('zTabs', function() {                    //TODO TODO TODO TODO TODO
-  return {
-    restrict: 'E',
-    transclude: true,
-    scope: {},
-    controller: ['$scope',"ItemsManipulator", function MyTabsController($scope,ItemsManipulator) {
-      $scope.im = ItemsManipulator;
-
-      $scope.panes = [];
-
-      $scope.select = function(pane) {
-        angular.forEach($scope.panes, function(pane) {
-          pane.selected = false;
-        });
-        $(".selectedTab").removeClass("selectedTab");
-
-
-        $("#tab-"+pane.index.toString()).addClass("selectedTab");
-        pane.selected = true;
-
-        if (pane.index == 1){
-          $scope.im.switchList("records");
-        }
-
-      };
-
-      this.addPane = function(pane) {
-        if ($scope.panes.length === 0) { // the first tab to add into $scope.panes is shown
-          $scope.select(pane);
-        }
-        $scope.panes.push(pane);
-      };
-    }],
-    template: z_tabsHTML
-  };
-})
-.directive('zPane', function() {                    //TODO TODO TODO TODO TODO
-  return {
-
-    restrict: 'E',
-    require: '^^zTabs',
-
-    scope: {
-      label: '@'
-    },
-
-    transclude: true,
-    link: function(scope, element, attrs, tabsCtrl) {
-      scope.index = attrs.index;
-      tabsCtrl.addPane(scope);
-    },
-    template: z_paneHTML
-  };
-});/**/
-
-//alert();
-//-------------------------------------------------------------------------------------
-CTapp.directive("zDebug",function(){                    //TODO TODO TODO TODO TODO
-  return {
-    restrict: "E",
-    scope:{},
-    controller: ["$scope",function ZDebugCtrler($scope){
-      $scope.mousePos = "( , )";
-      $scope.showDebugger = true;
-
-      $scope.toggleDebugP = function(){
-        $scope.showDebugger = !$scope.showDebugger;
-      }
-
-      $scope.test = function(a){
-        alert("debug test func w/ input: "+a);
-      }
-
-      this.updateMousePos = function(x,y){
-        $scope.$apply(function(){ // forcing the ng-digest-cycle to include mousePos' update
-            $scope.mousePos = strf("({},{})",[x,y]);
-        });
-      }
-    }],
-
-    require: "zDebug",
-    link: function(scope, element, attrs,debugCtrler){
-      	$(".zone").mousemove(function(event){
-      		debugCtrler.updateMousePos(event.offsetX,event.offsetY);
-      	});
-    },
-
-    template: z_debugHTML
-  };
 });
+
+//alert("factory loaded");
