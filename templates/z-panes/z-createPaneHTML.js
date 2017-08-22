e54253c26bb226bdf3a889a9fd21adbe2f255214
zion Niq <zion Niq>
2017-08-22 00:20:41 -0400

    updated the layouts of the two panes, they work as they are now but can look much better to improve UX

--- templates/z-panes/z-createPaneHTML.js
+++ templates/z-panes/z-createPaneHTML.js
@@ -1,12 +1,12 @@
 var z_createPaneHTML = ''+
 '<div class = "panel" ng-show = "ts.selectedTab == \'create\'">'+
   '<div class = "form-container zone">'+
-      '<a>{{im.cform.title}}</a>'+
+      '<a class = "formTitle YellowBG">{{im.cform.title}}</a>'+
       '<div class = "component-container">'+
         '<div class = "component zone" ng-repeat = "component in im.cform.components">'+
           '<a ng-click = "rmComponent($index)">X</a>'+
           '<div ng-repeat = "(key,val) in component.data">'+
-            '<a ng-click = "updateKey(key,component)">{{key}}</a>:<button>{{component.type}}</button>'+
+            '<a ng-click = "updateKey(key,component)">{{key}}</a>:<button>{{component.updateLabel}}</button>'+
           '</div>'+
         '</div>'+
       '</div>'+
--- templates/z-panes/z-viewerPaneHTML.js
+++ templates/z-panes/z-viewerPaneHTML.js
@@ -1,16 +1,20 @@
 var z_viewerPaneHTML = ''+
 '<div class = "panel" ng-show = "ts.selectedTab == \'viewer\'">'+
   '<div class = "form-container zone">'+
-      '<a>blabla</a>'+
-      '<a class = "zone yellowBG">{{cr.title}}</a>'+
-      /*
+      '<a class = "zone formTitle yellowBG"> {{im.cfolder.form.title}}</a>'+
+      '<br>'+
+      'Date Time: <a class = "zone yellowBG"> {{im.crecord.title}}</a>'+
+      '<br><br>'+
       '<div class = "component-container">'+
-        '<div class = "component zone" ng-repeat = "component in im.cform.components">'+
+        '<div class = "component zone" ng-repeat = "component in im.crecord.components">'+
           '<div ng-repeat = "(key,val) in component.data">'+
-            '<a>{{key}}</a>:<button ng-click = "updateVal(key)">{{component.type}}</button>'+
+            '<a ng-click = "alertTotVal(component)">{{key}}</a>'+
+            ':<button ng-click = "updateVal(component,key)">{{component.updateLabel}}</button>'+
+            '\xa0\xa0\xa0\xa0\xa0{{component.data[key]}}'+
           '</div>'+
         '</div>'+
-      '</div>'+*/
+      '</div>'+
+      '<textarea id="recordExtras" placeHolder = "[Extra Notes Here]"></textarea>'+
   '</div>'+
   '<button ng-click = "saveRecord()"> Save Record </button>'+
   '<button ng-click = "deleteRecord()"> Delete Record </button>'+
