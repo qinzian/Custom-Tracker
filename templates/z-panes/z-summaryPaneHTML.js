d761d563ef7eb13e44f7a771d22f5653bd13ef7f
zion Niq <zion Niq>
2017-09-01 18:44:28 -0400

    added a temporary template for summary

--- templates/z-panes/z-summaryPaneHTML.js
+++ templates/z-panes/z-summaryPaneHTML.js
@@ -1,4 +1,13 @@
 var z_summaryPaneHTML = ''+
 '<div class = "panel" ng-show = "ts.selectedTab == \'summary\'">'+
-'summarize stuff'+
+  '<button ng-click = "updateSum()"> Refresh </button>'+
+  '<div class = "summary-container">'+
+    '<a class = "zone formTitle yellowBG"> {{im.cfolder.getFormUsedTitle()}}</a>'+
+    //'<a class = "zone yellowBG"> {{im.crecord.getTitle()}}</a> <br><br>'+ //from what time to what time
+    '<div class = "component" ng-repeat = "component in im.cfolder.getTotals()">'+
+      '<div class = "zone" ng-show = "!isDefaultLabel(key)" ng-repeat = "(key,tot) in component.getData()">'+
+        '<a>{{key}}</a>\xa0\xa0:\xa0\xa0\xa0{{tot}}'+
+      '</div>'+
+    '</div>'+
+  '</div>'+
 '</div>';
